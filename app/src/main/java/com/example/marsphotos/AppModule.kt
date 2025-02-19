b9c12a8829861c7ae6391840198f1dfd2070a30f
GeraGarcia10 <s22120009@alumnos.itsur.edu.mx>
2025-02-19 00:13:38 -0600

    Se agrega un nuevo archivo donde se configura qué instancias se deben inyectar y cómo se deben crear.

---
+++ app/src/main/java/com/example/marsphotos/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.marsphotos
+
+import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.network.MarsApiService
+import com.example.marsphotos.network.MarsPhotosRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMarsApiService(): MarsApiService {
+        return MarsApi.retrofitService
+    }
+
+    @Provides
+    @Singleton
+    fun provideMarsPhotosRepository(apiService: MarsApiService): MarsPhotosRepository {
+        return MarsPhotosRepository(apiService)
+    }
+}
+
+class SingletonComponent {
+
+}
